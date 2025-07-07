697ee1836a178a445f77fb481c9df800766030f3
Ateto1202 <tonysu1204@gmail.com>
2025-07-07 09:28:56 +0800

    Remove the field of participants from task entity and update the task service

--- src/main/java/absent_minded/absent_minded/repositories/TaskRepository.java
+++ src/main/java/absent_minded/absent_minded/repositories/TaskRepository.java
@@ -1,5 +1,6 @@
 package absent_minded.absent_minded.repositories;
 
+import absent_minded.absent_minded.models.Project;
 import absent_minded.absent_minded.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -9,13 +10,6 @@ import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, String> {
     List<Task> findAllByOwnerId(String ownerId);
-    List<Task> findAllByOwnerIdAndProject(String ownerId, String project);
     List<Task> findAllByProject(String project);
-
-    @Transactional
-    @Modifying
-    void deleteAllByIdInAndOwnerId(Iterable<String> ids, String ownerId);
-
-    List<Task> findAllByParticipantsContains(String email);
-    List<Task> findAllByParticipantsContainsAndProject(String email, String project);
+    List<Task> findAllByProjectIn(List<String> projectIds);
 }

7932940313b57b571fad180320a7a1cd5bff3f64
Ateto1202 <tonysu1204@gmail.com>
2025-07-07 09:28:07 +0800

    Update the dependency injection to the integrate tests of project controller and user controller

--- src/test/java/absent_minded/absent_minded/controllers/UserControllerTest.java
+++ src/test/java/absent_minded/absent_minded/controllers/UserControllerTest.java
@@ -1,4 +1,3 @@
-// UserControllerTest.java
 package absent_minded.absent_minded.controllers;
 
 import absent_minded.absent_minded.models.User;
@@ -8,9 +7,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.server.ResponseStatusException;
@@ -26,9 +24,8 @@ import static org.springframework.http.HttpStatus.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@WebMvcTest(UserController.class)
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 public class UserControllerTest {
 
     @Autowired

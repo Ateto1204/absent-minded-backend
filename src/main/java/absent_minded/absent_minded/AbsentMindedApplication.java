86ecc23941c5c0f653c52288f7fa4ab022888abb
tyu <225jimmy@gmail.com>
2025-12-09 17:18:55 +0800

    create develop branch

--- src/main/java/absent_minded/absent_minded/AbsentMindedApplication.java
+++ src/main/java/absent_minded/absent_minded/AbsentMindedApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AbsentMindedApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println("=== ENV CHECK ===");
+        System.out.println("URL=" + System.getenv("SPRING_DATASOURCE_URL"));
+        System.out.println("USER=" + System.getenv("SPRING_DATASOURCE_USERNAME"));
+        System.out.println("PWD=" + System.getenv("SPRING_DATASOURCE_PASSWORD"));
 		SpringApplication.run(AbsentMindedApplication.class, args);
 	}
-
 }
